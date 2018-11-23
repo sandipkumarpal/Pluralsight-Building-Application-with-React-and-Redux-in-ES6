514bfa1cf8d5d67fbe295ba9f33fcf1291f3936b
Sandip Pal <sandip.pal18@gmail.com>
2018-11-23 23:12:46 +0530

    Adds reduxFlow End

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,6 +1,8 @@
+import {CREATE_COURSE} from '../actions/actionTypes';
+
 export default function courseReducer(state=[], action) {
   switch(action.type) {
-    case 'CREATE_COURSE':
+    case CREATE_COURSE:
       return [
         ...state,
         Object.assign({}, action.course)
